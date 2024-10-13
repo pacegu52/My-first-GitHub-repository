384ed9d50f36e01c96e80c38b751e0ae0c17ed59
pacegu52 <pcg520207@hotmail.com>
2024-10-12 18:26:21 -0600

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -3,3 +3,9 @@ This is my first code. It's just practice.
 #Display the output
 
 print("New Python File")
+
+This is the second code. for the Youth branch.
+
+#Display the output
+
+  print("Hello Mexico")
